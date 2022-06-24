6f35f8e9262146c2621d091ef7be2365cd6243f5
leiju <leijuxx@outlook.com>
2022-06-24 23:38:17 +0800

    添加测试

--- handle_test.go
+++ handle_test.go
@@ -11,7 +11,11 @@ import (
 	"github.com/tidwall/gjson"
 )
 
-var outBuffer *bytes.Buffer
+var (
+	outBuffer *bytes.Buffer
+
+	ErrTest = errors.New("test err")
+)
 
 func TestMain(m *testing.M) {
 	outBytes := make([]byte, 0, 1024)
@@ -20,28 +24,194 @@ func TestMain(m *testing.M) {
 	os.Exit(m.Run())
 }
 
-var ErrTest = errors.New("one err")
-
-func testResultErrFunc() error {
+func testHandleErrFunc() error {
 	return ErrTest
 }
 
-func TestResultErr(t *testing.T) {
+func TestHandleErr(t *testing.T) {
 	t.Run("add msg", func(t *testing.T) {
-		he := HandleErr(testResultErrFunc()).Msg("test msg")
+		he := HandleErr(testHandleErrFunc()).Msg("test msg")
 		assert.Equal(t, he.msg, "test msg")
+	})
+	t.Run("must err", func(t *testing.T) {
+		defer outBuffer.Reset()
+		defer func() {
+			if err := recover(); err != nil {
+				assert.Equal(t, err.(string), "must err")
+
+				json := gjson.ParseBytes(outBuffer.Bytes())
+				assert.Equal(t, json.Get("level").String(), "panic")
+				assert.Equal(t, json.Get("error").String(), ErrTest.Error())
+				assert.Equal(t, json.Get("Must").String(), "HandleErr err")
+				assert.Equal(t, json.Get("message").String(), "must err")
+			}
+		}()
+		HandleErr(testHandleErrFunc()).
+			Msg("must err").
+			Must()
 	})
 	t.Run("ignore err", func(t *testing.T) {
 		defer outBuffer.Reset()
 		HandleErr(
-			testResultErrFunc(),
+			testHandleErrFunc(),
 		).Msg("ignore err").Ignore()
 		//t.Log(outBuffer.String())
 		//{"level":"debug","error":"one err","Ignore":"HandleErr err","time":"2022-06-18T13:29:32+08:00","message":"ignore err"}
-		json := gjson.Parse(outBuffer.String())
+		json := gjson.ParseBytes(outBuffer.Bytes())
 		assert.Equal(t, json.Get("level").String(), "debug")
 		assert.Equal(t, json.Get("error").String(), ErrTest.Error())
 		assert.Equal(t, json.Get("Ignore").String(), "HandleErr err")
 		assert.Equal(t, json.Get("message").String(), "ignore err")
 	})
+}
+
+func testResultErrFunc(i int) (int, error) {
+	return i, ErrTest
+}
+
+func TestResultErr(t *testing.T) {
+	type args struct {
+		n int
+	}
+	tests := []struct {
+		name string
+		args args
+	}{
+		{
+			name: "1",
+			args: args{
+				n: 0,
+			},
+		},
+		{
+			name: "2",
+			args: args{
+				n: 100,
+			},
+		},
+		{
+			name: "3",
+			args: args{
+				n: -50,
+			},
+		},
+	}
+	for _, tt := range tests {
+		t.Run("add msg", func(t *testing.T) {
+			he := ResultErr(testResultErrFunc(tt.args.n)).Msg("test msg")
+
+			assert.Equal(t, he.msg, "test msg")
+			assert.Equal(t, he.result, tt.args.n)
+		})
+		t.Run("must err", func(t *testing.T) {
+			defer outBuffer.Reset()
+			defer func() {
+				if err := recover(); err != nil {
+					assert.Equal(t, err.(string), "must err")
+
+					json := gjson.ParseBytes(outBuffer.Bytes())
+					assert.Equal(t, json.Get("level").String(), "panic")
+					assert.Equal(t, json.Get("error").String(), ErrTest.Error())
+					assert.Equal(t, json.Get("Must").String(), "ResultErr err")
+					assert.Equal(t, json.Get("message").String(), "must err")
+				}
+			}()
+			ResultErr(testResultErrFunc(tt.args.n)).
+				Msg("must err").
+				Must()
+		})
+		t.Run("ignore err", func(t *testing.T) {
+			defer outBuffer.Reset()
+			r := ResultErr(
+				testResultErrFunc(tt.args.n),
+			).Msg("ignore err").Ignore()
+
+			assert.Equal(t, r, tt.args.n)
+
+			json := gjson.ParseBytes(outBuffer.Bytes())
+			assert.Equal(t, json.Get("level").String(), "debug")
+			assert.Equal(t, json.Get("error").String(), ErrTest.Error())
+			assert.Equal(t, json.Get("Ignore").String(), "ResultErr err")
+			assert.Equal(t, json.Get("message").String(), "ignore err")
+		})
+	}
+}
+
+func testResultsErrFunc(i int, s string) (int, string, error) {
+	return i, s, ErrTest
+}
+
+func TestResultsErr(t *testing.T) {
+	type args struct {
+		n   int
+		str string
+	}
+	tests := []struct {
+		name string
+		args args
+	}{
+		{
+			name: "1",
+			args: args{
+				n:   0,
+				str: "",
+			},
+		},
+		{
+			name: "2",
+			args: args{
+				n:   100,
+				str: "abcdefg",
+			},
+		},
+		{
+			name: "3",
+			args: args{
+				n:   -50,
+				str: "123584%$#@^&",
+			},
+		},
+	}
+	for _, tt := range tests {
+		t.Run("add msg", func(t *testing.T) {
+			he := ResultsErr(testResultsErrFunc(tt.args.n, tt.args.str)).Msg("test msg")
+
+			assert.Equal(t, he.msg, "test msg")
+			assert.Equal(t, he.result1, tt.args.n)
+			assert.Equal(t, he.result2, tt.args.str)
+		})
+		t.Run("must err", func(t *testing.T) {
+			defer outBuffer.Reset()
+			defer func() {
+				if err := recover(); err != nil {
+					assert.Equal(t, err.(string), "must err")
+
+					json := gjson.ParseBytes(outBuffer.Bytes())
+					assert.Equal(t, json.Get("level").String(), "panic")
+					assert.Equal(t, json.Get("error").String(), ErrTest.Error())
+					assert.Equal(t, json.Get("Must").String(), "ResultsErr err")
+					assert.Equal(t, json.Get("message").String(), "must err")
+				}
+			}()
+			ResultsErr(testResultsErrFunc(tt.args.n, tt.args.str)).
+				Msg("must err").
+				Must()
+		})
+		t.Run("ignore err", func(t *testing.T) {
+			defer outBuffer.Reset()
+			r1, r2 := ResultsErr(
+				testResultsErrFunc(tt.args.n, tt.args.str),
+			).Msg("ignore err").Ignore()
+
+			assert.Equal(t, r1, tt.args.n)
+			assert.Equal(t, r2, tt.args.str)
+
+			json := gjson.ParseBytes(outBuffer.Bytes())
+			assert.Equal(t, json.Get("level").String(), "debug")
+			assert.Equal(t, json.Get("error").String(), ErrTest.Error())
+			assert.Equal(t, json.Get("Ignore").String(), "ResultsErr err")
+			assert.Equal(t, json.Get("message").String(), "ignore err")
+		})
+	}
+
 }
